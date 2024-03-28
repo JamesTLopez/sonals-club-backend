03a006e64d8d5026c50f94bbbbf42c1cc6af9bc0
James Lopez <James.Theo.Lopez@gmail.com>
2024-03-28 15:53:33 -0400

    add chi and dockerfile.dev file

--- main.go
+++ main.go
@@ -1,20 +1,20 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
-	"net/http"
+	"sonalsguild/api/application"
 )
 
-func homePage (w http.ResponseWriter,r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the jungle!");
-	fmt.Printf("home page endpoint")
-
-}
-
 func main() {
-	log.Println("Hai")
-	fmt.Println("FALS")
-	http.HandleFunc( "/", homePage)
-	log.Fatal(http.ListenAndServe(":8080",nil))
+	log.Println("Starting...")
+	app := application.New()
+
+	err := app.Start(context.TODO())
+
+	if err != nil {
+		fmt.Printf("Error starting server %v",err)
+	}
 }
+
