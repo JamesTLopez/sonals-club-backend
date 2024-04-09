92e558ffd38c20ccba5abffe964ca7ada9eb670c
James Lopez <James.Theo.Lopez@gmail.com>
2024-04-09 15:10:47 -0400

    feat: simple song service init

---
+++ album-reviews/services/Songs.go
@@ -0,0 +1,73 @@
+package services
+
+import (
+	"context"
+	"time"
+)
+
+type Song struct {
+	ID string `json:"id"`
+	User_id string `json:"user_id"` // TODO
+	Name string `json:"name"`
+	Description string `json:"description"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"` // TODO
+}
+
+func (s *Song) GetAllSongs() ([]*Song,error) {
+	ctx, cancel := context.WithTimeout(context.Background(),dbTimeout)
+	defer cancel()
+
+
+	query := `SELECT id, name, description, created_at FROM songs`
+
+	rows,err := db.QueryContext(ctx, query)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var songs []*Song
+	for rows.Next() {
+		var song Song 
+		err := rows.Scan(
+			&song.ID,
+			&song.Name,
+			&song.Description,
+			&song.CreatedAt,
+		)
+
+		if err != nil {
+			return nil,err
+		}
+
+		songs = append(songs, &song)
+	}
+
+	return songs,nil
+}
+
+
+func (s *Song) CreateSong(song Song) (*Song,error) {
+	ctx, cancel := context.WithTimeout(context.Background(),dbTimeout)
+
+	defer cancel()
+	query := `
+		INSERT INTO songs (name,description) 
+		VALUES ($1,$2) return *
+	`
+	 
+	_, err := db.ExecContext(
+		ctx,
+		query,
+		song.Name,
+		song.Description)
+	
+	if err != nil {
+		return nil, err
+	}
+	
+
+	return &song,nil
+
+}
