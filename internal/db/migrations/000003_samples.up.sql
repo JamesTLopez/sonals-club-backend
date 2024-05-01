e5740733001741ef90307f3b21181664fa241dc2
James Lopez <James.Theo.Lopez@gmail.com>
2024-05-01 16:22:17 -0400

    feat: create migrations for samples and song data tables

--- internal/db/migrations/000003_samples.up.sql
+++ internal/db/migrations/000003_samples.up.sql
@@ -1,12 +1,10 @@
 
-CREATE TABLE IF NOT EXISTS Songs(
+CREATE TABLE IF NOT EXISTS Samples(
    id serial PRIMARY KEY,
    user_id INT REFERENCES users(id),
+   song_id INT REFERENCES songs(id),
    name VARCHAR (50) NOT NULL,
-   labels VARCHAR (50),
-   description VARCHAR (50) NOT NULL,
-   duration INT,
    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
--- TODO: onces file upload is created, add new column for s3 image
+-- TODO: onces file upload is created, add new column for s3
