e65e7f72da0bee386947ab60a5dd2fed11bfd155
JamesTLopez <james.theo.lopez@gmail.com>
2024-06-17 15:06:41 -0400

    refactor: rearrange backend

--- internal/controllers/auth.go
+++ internal/controllers/auth.go
@@ -1,7 +1,11 @@
 package controllers
 
 import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"sonalsguild/helpers"
@@ -86,4 +90,55 @@ func GetAuthCallbackSpotify(w http.ResponseWriter, r *http.Request) {
 	}
 
 	helpers.WriteJson(w, http.StatusOK, helpers.Envelop{"authToken":token})
+}
+
+
+func RetrieveSpotifyAuthorizationToken(state string, code string) (*AuthResponse, error) {
+	url := "https://accounts.spotify.com/api/token"
+	client_id := os.Getenv("SPOTIFY_CLIENT_ID")
+	spotify_secret := os.Getenv("SPOTIFY_SECRET")
+	redirect_uri := os.Getenv("SPOTIFY_REDIRECT")
+
+
+
+	// Prepare the authorization header
+	authHeader := base64.StdEncoding.EncodeToString([]byte(client_id + ":" + spotify_secret))
+	reqBody := fmt.Sprintf("code=%s&redirect_uri=%s&grant_type=authorization_code", code, redirect_uri)
+
+	// Create a new HTTP request
+	req, err := http.NewRequest("POST", url, bytes.NewBufferString(reqBody))
+	if err != nil {
+		fmt.Println("Error generating new request", err)
+		return nil,err
+	}
+	// Set headers
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Authorization", "Basic " + authHeader)
+
+	// Send the request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err!= nil {
+		fmt.Println("Error retrieving user information token:", err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	// Read the response body and ensure it does not have any errors
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error retrieving user information token:", err)
+		return nil, err
+	}
+
+	// Decode JSON response
+	var authResp AuthResponse
+	err = json.Unmarshal(body, &authResp)
+
+	if err != nil {
+		fmt.Println("Error decoding JSON:", err)
+		return nil, err
+	}
+
+	return &authResp,nil
 }
--- internal/controllers/spotify.go
+++ internal/controllers/spotify.go
@@ -1,14 +1,11 @@
 package controllers
 
 import (
-	"bytes"
-	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
-	"os"
 	"sonalsguild/helpers"
 )
 
@@ -63,52 +60,3 @@ func GetSpotifyUser(auth AuthResponse) (*GetUserSpotify, error) {
 	return &userRepsonse, nil
 }
 
-func RetrieveSpotifyAuthorizationToken(state string, code string) (*AuthResponse, error) {
-	url := "https://accounts.spotify.com/api/token"
-	client_id := os.Getenv("SPOTIFY_CLIENT_ID")
-	spotify_secret := os.Getenv("SPOTIFY_SECRET")
-	redirect_uri := os.Getenv("SPOTIFY_REDIRECT")
-
-
-
-	// Prepare the authorization header
-	authHeader := base64.StdEncoding.EncodeToString([]byte(client_id + ":" + spotify_secret))
-	reqBody := fmt.Sprintf("code=%s&redirect_uri=%s&grant_type=authorization_code", code, redirect_uri)
-
-	// Create a new HTTP request
-	req, err := http.NewRequest("POST", url, bytes.NewBufferString(reqBody))
-	if err != nil {
-		fmt.Println("Error generating new request", err)
-		return nil,err
-	}
-	// Set headers
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	req.Header.Set("Authorization", "Basic " + authHeader)
-
-	// Send the request
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err!= nil {
-		fmt.Println("Error retrieving user information token:", err)
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	// Read the response body and ensure it does not have any errors
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Error retrieving user information token:", err)
-		return nil, err
-	}
-
-	// Decode JSON response
-	var authResp AuthResponse
-	err = json.Unmarshal(body, &authResp)
-
-	if err != nil {
-		fmt.Println("Error decoding JSON:", err)
-		return nil, err
-	}
-
-	return &authResp,nil
-}
